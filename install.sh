f6320a879b20bcb60b6b59c26f7448479e9a4621
Niccolo Dejito <ncdejito@gmail.com>
2022-11-03 12:51:09 +0800

    Add comments

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ apt update
 apt install -y tmux
 apt install -y vim
 
+# ros packages
 apt install -y ros-galactic-navigation2
 apt install -y ros-galactic-nav2-bringup
 apt install -y ros-galactic-turtlebot3*
@@ -16,10 +17,12 @@ echo "export TURTLEBOT3_MODEL=waffle" >> ~/.bashrc
 echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/galactic/share/turtlebot3_gazebo/models" >> ~/.bashrc
 source ~/.bashrc
 
+# lidar
 apt install -y ros-galactic-rplidar-ros
 cd /opt/ros/galactic/share/rplidar_ros/launch/
 sed 's/laser/lidar_link/g' rplidar.launch.py > rplidar.launch.py
 
+# robot description
 cd ~
 mkdir -p ros_ws/src
 cd ~/ros_ws/src
@@ -27,6 +30,7 @@ git clone https://github.com/ros-planning/navigation2_tutorials/
 mv navigation2_tutorials/sam_bot_description/ .
 rm -r navigation2_tutorials/
 
+# odometry
 cd ~/ros_ws/src
 git clone https://github.com/Adlink-ROS/rf2o_laser_odometry
 cd rf2o_laser_odometry/launch/
